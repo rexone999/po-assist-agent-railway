5775fd6b9102a18b1562421b68011e58aae0baee
rexone999 <projectmajorvd25@gmail.com>
2025-06-10 15:28:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { chat } = require('./utils/gemini');
+const { chat } = require('./utils/openrouter');
 require('dotenv').config();
 
 const app = express();
